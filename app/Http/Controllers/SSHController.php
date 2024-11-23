43d9b31f632be996281c223faace4a1da777af9e
Maksim <m1xg4me@yandex.ru>
2024-11-23 18:15:19 +0300

    add SSH method

--- app/Http/Controllers/SSHController.php
+++ app/Http/Controllers/SSHController.php
@@ -6,43 +6,8 @@ use Illuminate\Http\Request;
 
 class SSHController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function sshFromGitLaravel2()
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return "";
     }
 }
